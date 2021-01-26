1cb6b6bc7b84788fb5e01e7358f53820142b31b8
a-poor <45295232+a-poor@users.noreply.github.com>
2021-01-26 15:21:23 -0500

    Updating flask app

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,12 +4,25 @@ import make_palette
 
 app = Flask(__name__)
 
+def rgb2hex(r,g,b):
+    return f"{r:02x}{g:02x}{b:02x}"
+
+def make_palette_url(palette):
+    return "https://coolors.co/" + "-".join(rgb2hex(*c) for c in (palette))
+
 @app.route("/")
 def main():
     imgurl = request.args.get("url")
     if imgurl is None: return "No url specified"
     clusters = make_palette.url_to_clusters(imgurl)
     return json.dumps({"clusters":clusters})
+
+@app.route("/palette-url")
+def palette_url():
+    imgurl = request.args.get("url")
+    if imgurl is None: return "No url specified"
+    clusters = make_palette.url_to_clusters(imgurl)
+    return json.dumps({"palette_url":make_palette_url(clusters)})
 
 
 if __name__ == "__main__":
