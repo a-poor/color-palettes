30c1805f83680a92522eb7bbf81aa3e12ee00a27
a-poor <45295232+a-poor@users.noreply.github.com>
2021-01-27 13:09:34 -0500

    Adding filter_fn arg to color_palettes.make_all_palettes

--- color_palettes.py
+++ color_palettes.py
@@ -10,21 +10,33 @@ from PIL import Image
 from skimage import color
 from sklearn.cluster import KMeans, AgglomerativeClustering
 
-from typing import Union
+from typing import Union, Callable
 
 
 def read_image(path: Union[str,Path]) -> np.ndarray:
     with open(path,"rb") as f:
         return np.array(Image.open(f))
 
-def preprocess_image(img: np.ndarray) -> : np.ndarray:
-    return img.reshape((-1,3)).astype("float32") / 255
-
 def get_kmeans_centers(img: np.ndarray, nclusters: int) -> np.ndarray:
     return KMeans(n_clusters=nclusters).fit(img).cluster_centers_
 
 def get_agglom_centers(img: np.ndarray, nclusters: int) -> np.ndarray:
     return AgglomerativeClustering(n_clusters=nclusters).fit(img).cluster_centers_
+    
+def rgb2hsv(dat: np.ndarray) -> np.ndarray:
+    return color.rgb2hsv([dat])[0]
+
+def hsv2rgb(dat: np.ndarray) -> np.ndarray:
+    return color.hsv2rgb([dat])[0]
+
+def preprocess_image(img: np.ndarray) -> : np.ndarray:
+    return img.reshape((-1,3)).astype("float32") / 255
+
+def preprocess_rgb(img: np.ndarray) -> np.ndarray:
+    return img.reshape((-1,3)).astype("float32") / 255
+
+def preprocess_hsv(img: np.ndarray) -> np.ndarray:
+    return rgb2hsv(preprocess_rgb(img))
 
 def plot_image(img: np.ndarray):
     plt.figure(figsize=(14,8))
@@ -41,28 +53,20 @@ def plot_palette(centers: np.ndarray):
     plt.grid()
     plt.axis('off')
     plt.show()
-    
-    
-def rgb2hsv(dat: np.ndarray) -> np.ndarray:
-    return color.rgb2hsv([dat])[0]
 
-def hsv2rgb(dat: np.ndarray) -> np.ndarray:
-    return color.hsv2rgb([dat])[0]
-
-def preprocess_rgb(img: np.ndarray) -> np.ndarray:
-    return img.reshape((-1,3)).astype("float32") / 255
-
-def preprocess_hsv(img: np.ndarray) -> np.ndarray:
-    return rgb2hsv(preprocess_rgb(img))
-
-
-def make_all_palettes(path: Union[str,Path], nclusters: int = 8):
+def make_all_palettes(path: Union[str,Path], nclusters: int = 8, filter_fn: Callable[[np.ndarray],np.ndarray] = None):
     # Load the image
     img = read_image(path)
     
     # Reshape and set range
-    rgb_pixels = preprocess_rgb(img)
-    hsv_pixels = preprocess_hsv(img)
+    rgb_pixels = preprocess_image(img)
+    
+    # If filter_fn is set, filter pixels
+    if filter_fn is not None:
+        rgb_pixels = filter_fn(rgb_pixels)
+    
+    # Convert RGB to HSV
+    hsv_pixels = rgb2hsv(rgb_pixels)
     
     # Cluster the pixels
     km_rgb_centers = get_kmeans_centers(rgb_pixels,nclusters)
